7af81ff577d5a7cf5bc3bbc278fbaea6986f8bd6
QiaoBenlong <2534112250@qq.com>
2025-07-15 17:45:02 +0800

    完美的workspace

--- 2025_work2_test1_0 copy/Debug/User/subdir_rules.mk
+++ 2025_work2_test1_0 copy/Debug/User/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 User/%.o: ../User/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs2001/ccs/tools/compiler/ti-cgt-armllvm_4.0.1.LTS/bin/tiarmclang.exe" -c @"syscfg/device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"D:/Git/example/2025_work2_test1_0 copy" -I"D:/Git/example/2025_work2_test1_0 copy/Debug" -I"C:/ti/mspm0_sdk_1_30_00_03/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_1_30_00_03/source" -I"D:/Git/example/2025_work2_test1_0 copy/User" -gdwarf-3 -MMD -MP -MF"User/$(basename $(<F)).d_raw" -MT"$(@)" -I"D:/Git/example/2025_work2_test1_0 copy/Debug/syscfg"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"C:/ti/ccs2020/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang.exe" -c @"syscfg/device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"D:/git/example/2025_work2_test1_0 copy" -I"D:/git/example/2025_work2_test1_0 copy/Debug" -I"C:/ti/mspm0_sdk_2_01_00_03/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_2_01_00_03/source" -I"D:/git/example/2025_work2_test1_0 copy/User" -gdwarf-3 -MMD -MP -MF"User/$(basename $(<F)).d_raw" -MT"$(@)" -I"D:/git/example/2025_work2_test1_0 copy/Debug/syscfg"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
